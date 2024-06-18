c36a1d6e9c619d6270355fa11fb05673394b72af
unknown <ericjackwong@outlook.com>
2024-06-18 19:01:17 -0400

    fixed the posts

--- assets/script/blogPage.js
+++ assets/script/blogPage.js
@@ -6,7 +6,9 @@ const postEl = document.querySelector('.blogPosts'); //returns all div's with bl
 
 //check for old posts, update board with new posts
 let postList = []; //scope issues if put in init
+
 function init(){
+
     if(!(localStorage.getItem("postList") === null)){ // if not post dne, so if the postList exists
         postList = JSON.parse(localStorage.getItem("postList"));
     }
@@ -16,6 +18,8 @@ function init(){
         localStorage.removeItem("newPost"); //clear post so if there is a reload, it does double add
         localStorage.setItem("postList", JSON.stringify(postList)); //add the new item to the list
     }
+
+    postBlogs();
 }
 
 
@@ -29,18 +33,40 @@ function createListItem(listItem){
     //each item has an username, a title and content
     //header- title, content, footer-username
     //i can set the div class to card to keep it contained
-    const newItem = document.createElement("li");
-    console.log(listItem.postTitle);
-    newItem.setAttribute("header", listItem.title);
-    newItem.setAttribute("footer", listItem.username);
-    newItem.textContent=listItem.content;
+    const newItem = document.createElement("div");
+    newItem.setAttribute("class", "card");
+
+    //each element needs to be added as a child
+
+    const newHeader = document.createElement("header");
+    newHeader.textContent = listItem.title;
+    newItem.appendChild(newHeader);
+    
+    const newContent = document.createElement("p");
+    newContent.textContent = listItem.content;
+    newItem.appendChild(newContent);
+
+    const newFooter = document.createElement("footer");
+    newFooter.textContent = listItem.username;
+    newItem.appendChild(newFooter);
+    
+    
     return newItem; //outputs an item to be added to the page
 }
 //populate the list
 
+function postBlogs(){
+    for (item of postList){ //go through the list
+        const newBlog = createListItem(item); //add each item to the blog
+        // console.log(typeof(item)); //item is an object
+        postEl.appendChild(newBlog);
+    }
+}
 
 
-const checker = createListItem(JSON.parse(localStorage.getItem("newPost")));
-// console.log(checker);
-postEl.appendChild(checker);
-// init();
+
+// const checker = createListItem(JSON.parse(localStorage.getItem("newPost")));
+// // console.log(checker);
+
+// postEl.appendChild(checker);
+init();
